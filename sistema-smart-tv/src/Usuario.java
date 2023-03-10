48e82768ccafde5cdb1237a29da31a98faff38a2
TheV1k <victormoreiramos@gmail.com>
2023-03-10 15:37:57 -0300

    first commit

--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -3,8 +3,26 @@ public class Usuario {
 
         SmartTv smartTv = new SmartTv();
 
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume Atual: " + smartTv.volume);
+
         System.out.println("A TV está ligada? " + smartTv.ligada);
         System.out.println("Canal atual:" + smartTv.canal);
         System.out.println("Volume atual: " + smartTv.volume);
+
+        System.out.println("Canal atual: " + smartTv.canal);
+
+        smartTv.mudarCanal(13);
+
+        System.out.println("Canal atual: " + smartTv);
+
+        smartTv.ligar();
+        System.out.println("Novo Status -->" + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo Status -->" + smartTv.ligada);
     }
 }
